dcd8c54e16c4ac4e0193ae2721b2b6a9d646d543
Batflash2 <69955149+Batflash2@users.noreply.github.com>
2021-02-19 13:20:30 +0530

    Can display the Product title, availability, mrp and deal price when the url is entered.

--- Amazon-Tracker.py
+++ Amazon-Tracker.py
@@ -1,9 +1,9 @@
 import requests
-import selectorlib
 from bs4 import BeautifulSoup
 
-# url = input("Please enter the url\n")
-url = 'https://www.amazon.in/Infinity-Glide-500-Wireless-Headphones/dp/B07W5MYRF4/ref=sr_1_1_sspa?crid=384417BZYUPRB&dchild=1&keywords=headphone+wireless&qid=1613395289&smid=A14CZOWI0VEHLG&sprefix=headph%2Caps%2C-1&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExVkxNS1MwTkpGUVhWJmVuY3J5cHRlZElkPUEwNzc4NTExMTNTRkVZUTVHRzFTWCZlbmNyeXB0ZWRBZElkPUEwOTM4NDM1M0Y0VVYzUUhEUlhSOCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
+url = input("Please enter the url\n")
+# url = 'https://www.amazon.in/Infinity-Glide-500-Wireless-Headphones/dp/B07W5MYRF4/ref=sr_1_1_sspa?crid=384417BZYUPRB&dchild=1&keywords=headphone+wireless&qid=1613395289&smid=A14CZOWI0VEHLG&sprefix=headph%2Caps%2C-1&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExVkxNS1MwTkpGUVhWJmVuY3J5cHRlZElkPUEwNzc4NTExMTNTRkVZUTVHRzFTWCZlbmNyeXB0ZWRBZElkPUEwOTM4NDM1M0Y0VVYzUUhEUlhSOCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
+# url = 'https://www.amazon.com/Sony-MDRZX110-BLK-Stereo-Headphones/dp/B00NJ2M33I/ref=sr_1_3?dchild=1&keywords=headphones&qid=1613717798&sr=8-3'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
@@ -14,20 +14,33 @@ headers = {
     'Connection': 'close'
 }
 
-#headers={'User-Agent': 'Defined'}
+print("Please wait. we are fetching the data\n\n")
 
 # Get the code of the product page using the url provided
 response = requests.get(url, headers=headers)
 
-print(response)
-
 # code proceeds only if the connection to the product page is successful
 if response:
-    print("Connected successfully")
+    print("Connected successfully\n\n")
 else:
     print("Connection failed")
     exit()
 
-soup = BeautifulSoup(response.content, 'html.parser').encode('utf-8 ')
-with open('product.txt', 'w', encoding='utf-8') as f:
-    f.write(response.text)
+soup = BeautifulSoup(response.content, 'lxml')
+
+# with open('product2.txt', 'w', encoding='utf-8') as f:
+#     f.write(soup.prettify())
+
+# Extracting data from the amazon html code
+ProductTitle = soup.find(id='productTitle').get_text().strip()
+Availability = soup.find(id='availability').get_text().strip()  # In stock.
+
+Price = soup.find(id='price')
+Mrp = int(soup.find(class_='priceBlockStrikePriceString a-text-strike').get_text().strip()[2:-3].replace(',', ''))
+DealPrice = int(soup.find(id='priceblock_dealprice').get_text().strip()[2:-3].replace(',', ''))
+
+# print(Price.prettify())
+print('Product Title: ', ProductTitle)
+print('Availability: ', Availability)
+print('MRP =', Mrp)
+print('Deal Price =', DealPrice)
