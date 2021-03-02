400feeb03ef4a6c785fa83a132f1fc240d35a68b
Prateek Rao <73730958+r-prateek@users.noreply.github.com>
2021-03-03 01:57:20 +0530

    created message with all fields

--- OtherFunctions/Send_SMS.py
+++ OtherFunctions/Send_SMS.py
@@ -9,3 +9,10 @@ auth_token = 'c873b6301629c94ea63f50ba2bec0818'
 
 client = Client(account_sid, auth_token)
 
+#Writing a message
+
+message = client.messages.create(
+    from_= '+14124538097',
+    to = '+919342581008',
+    body = 'This is a test for python sms'
+)
