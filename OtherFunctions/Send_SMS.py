7c65fcc0e8f9cdb22ba4946ecb24389f65827db3
Prateek Rao <73730958+r-prateek@users.noreply.github.com>
2021-03-03 01:53:49 +0530

    initialized sid & auth token

---
+++ OtherFunctions/Send_SMS.py
@@ -0,0 +1,9 @@
+#importing twilio client
+
+from twilio.rest import Client
+
+#Taking Account SID and authentication token from twilio.com
+
+account_sid = 'AC2b9b3923e7391dd37e6cf793dde22e3b'
+auth_token = 'c873b6301629c94ea63f50ba2bec0818'
+
