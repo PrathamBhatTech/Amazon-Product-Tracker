ae7f05db3c8ac6b835729ea36b3a1673aff0ba84
Prateek Rao <73730958+r-prateek@users.noreply.github.com>
2021-03-03 12:32:14 +0530

    created function for sending message

--- OtherFunctions/Send_SMS.py
+++ OtherFunctions/Send_SMS.py
@@ -4,17 +4,19 @@ from twilio.rest import Client
 import os
 #Taking Account SID and authentication token from twilio.com
 
-account_sid = 'AC2b9b3923e7391dd37e6cf793dde22e3b'
-auth_token = '73e05c2e1af8d22379cb305d0d55cddd'
+def send_sms(number)
+    account_sid = 'AC2b9b3923e7391dd37e6cf793dde22e3b'
+    auth_token = '73e05c2e1af8d22379cb305d0d55cddd'
 
-client = Client(account_sid, auth_token)
+    client = Client(account_sid, auth_token)
 
-#Writing a message
+    #Writing a message
 
-message = client.messages.create(
-    from_= '+14124538097',
-    to = '+919342581008',
-    body = 'This is a test for python sms'
-)
+    message = client.messages.create(
+        from_= '+14124538097',
+        to = '+919342581008',
+        body = 'This is a test for python sms'
+    )   
 
-print(message.sid)
+    #print(message.sid)
+
