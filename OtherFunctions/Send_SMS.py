4035ec7a4a003e71d61a122bc951affe585026a5
Prateek Rao <73730958+r-prateek@users.noreply.github.com>
2021-03-03 17:39:54 +0530

    updated requirements

--- OtherFunctions/Send_SMS.py
+++ OtherFunctions/Send_SMS.py
@@ -4,7 +4,7 @@ from twilio.rest import Client
 import os
 #Taking Account SID and authentication token from twilio.com
 
-def send_sms(number)
+def send_sms(number):
     account_sid = 'AC2b9b3923e7391dd37e6cf793dde22e3b'
     auth_token = '73e05c2e1af8d22379cb305d0d55cddd'
 
@@ -15,8 +15,9 @@ def send_sms(number)
     message = client.messages.create(
         from_= '+14124538097',
         to = '+919342581008',
-        body = 'This is a test for python sms'
+        body = 'Your amazon product is tracked'
     )   
 
     #print(message.sid)
 
+send_sms(number = '+919342581008')
