a9ecd55219a89e3ac6d559232e37b52ca230bddf
Prateek Rao <73730958+r-prateek@users.noreply.github.com>
2021-03-04 10:10:48 +0530

    updated function to take in number and send message

--- OtherFunctions/Send_SMS.py
+++ OtherFunctions/Send_SMS.py
@@ -1,12 +1,11 @@
 #importing twilio client
 
 from twilio.rest import Client
-import os
 #Taking Account SID and authentication token from twilio.com
 
 def send_sms(number):
     account_sid = 'AC2b9b3923e7391dd37e6cf793dde22e3b'
-    auth_token = '73e05c2e1af8d22379cb305d0d55cddd'
+    auth_token = '56a580fdbe1c8d88fdb9768992f55af4'
 
     client = Client(account_sid, auth_token)
 
@@ -14,10 +13,10 @@ def send_sms(number):
 
     message = client.messages.create(
         from_= '+14124538097',
-        to = '+919342581008',
-        body = 'Your amazon product is tracked'
+        to = 'number',
+        with open('OtherFunctions/mail_message.md', 'r+') as f:
+            body = f.read.format(name, product[0:3], price)
     )   
 
-    #print(message.sid)
 
-send_sms(number = '+919342581008')
+send_sms(number)
