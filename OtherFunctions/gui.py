54830437d77d90916480fc0b732801575a881c9c
Prateek Rao <73730958+r-prateek@users.noreply.github.com>
2021-03-05 13:50:10 +0530

    settings button

--- OtherFunctions/gui.py
+++ OtherFunctions/gui.py
@@ -1,7 +1,8 @@
 from tkinter import *
 
 root = Tk()
-main_label = Label(root, text = 'Amazon Tracker').pack() #to make bold & color maybe
+title = root.title('Amazon Tracker')
+main_label = Label(root, text = 'Amazon Tracker').place(x = 120, y = 0) #to make bold & color maybe
 
-
+settings_button = Button(root, text = '⚙️').place(x = 400, y = 0)
 root.mainloop()
