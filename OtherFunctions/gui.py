e480dea56c79a425473cbbb3047fa2e69424d1a7
Pratham Bhat <69955149+PrathamBhatTech@users.noreply.github.com>
2021-03-05 14:15:01 +0530

    Delete gui.py

--- OtherFunctions/gui.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-root = Tk()
-title = root.title('Amazon Tracker')
-main_label = Label(root, text = 'Amazon Tracker').place(x = 120, y = 0) #to make bold & color maybe
-
-settings_button = Button(root, text = '⚙️').place(x = 400, y = 0)
-root.mainloop()
