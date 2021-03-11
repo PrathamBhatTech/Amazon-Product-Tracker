9f189674dde00a587804f9768b298cf4104e4aba
Batflash2 <69955149+Batflash2@users.noreply.github.com>
2021-03-11 15:21:48 +0530

    Fixed error when product tracked becomes deal of the day.

--- OtherFunctions/Send_SMS.py
+++ OtherFunctions/Send_SMS.py
@@ -5,7 +5,7 @@ from twilio.rest import Client
 
 def send_sms(name, product, price, number):
     account_sid = 'AC2b9b3923e7391dd37e6cf793dde22e3b'
-    auth_token = ''
+    auth_token = '56a580fdbe1c8d88fdb9768992f55af4'
 
     client = Client(account_sid, auth_token)
     #Writing a message
