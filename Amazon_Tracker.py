6ec1840b0dcc05981bc19153c444a197b09ab2cb
PrathamBhatTech <bhat2002.prathham@gmail.com>
2022-11-14 08:57:11 +0530

    Changed log path to home

--- Amazon_Tracker.py
+++ Amazon_Tracker.py
@@ -24,7 +24,7 @@ from OtherFunctions.Send_Email import send_mail
 
 
 logging.basicConfig(
-    filename='amazon_product_tracker.log',
+    filename='$Home/amazon_product_tracker.log',
     level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     filemode='a'
